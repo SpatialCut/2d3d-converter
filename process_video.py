b3ab413cdd9f09b2faaad426e14f626923e07d95
feiwu <wufei523@gmail.com>
2024-04-24 16:08:22 -0700

    v2

--- process_video.py
+++ process_video.py
@@ -51,9 +51,9 @@ logging.basicConfig(
     ]
 )
 
-def depth_loss_function(y_true, y_pred, theta=0.1, maxDepthVal=1000.0 / 10.0):
-    # vggloss
-    lvgg = K.mean(K.abs(lossmodel(ytrue) - lossmodel(ypred)))
+def depth_loss_function(y_true, y_pred, theta=0.1, maxDepthVal=1000.0/10.0):
+    #vggloss
+    lvgg=K.mean(K.abs(lossmodel(ytrue)-lossmodel(ypred)))
     # Point-wise depth
     l_depth = K.mean(K.abs(y_pred - y_true), axis=-1)
 
@@ -69,24 +69,22 @@ def depth_loss_function(y_true, y_pred, theta=0.1, maxDepthVal=1000.0 / 10.0):
     w1 = 1.0
     w2 = 1.0
     w3 = theta
-    w4 = 0.1
+    w4=0.1
 
-    return (w1 * l_ssim) + (w2 * K.mean(l_edges)) + (w3 * K.mean(l_depth)) + w4 * lvgg
+    return (w1 * l_ssim) + (w2 * K.mean(l_edges)) + (w3 * K.mean(l_depth))+w4*lvgg
 
-
-# keras.backend.normalize_data_format=normalize_data_format
+#keras.backend.normalize_data_format=normalize_data_format
 def ssim_loss(y_true, y_pred):
     l_ssim = K.clip((1 - tf.image.ssim(y_true, y_pred, 1)) * 0.5, 0, 1)
-    # vggloss
-    lvgg = K.clip(K.mean(K.abs(lossmodel(y_true) - lossmodel(y_pred))), 0, 1)
-    return l_ssim + tf.keras.losses.MAE(y_true, y_pred) + lvgg
+    #vggloss
+    lvgg=K.clip(K.mean(K.abs(lossmodel(y_true)-lossmodel(y_pred))),0,1)
+    return l_ssim+tf.keras.losses.MAE(y_true, y_pred)+lvgg
 
-
-def animate(U, D, savename='test', show=False):
-    ims = []
+def animate(U,D, savename='test',show=False):
+    ims=[]
     fig = plt.figure()
     for i in range(10):
-        if i % 2 == 0:
+        if i%2==0:
             im = plt.imshow(U, animated=True)
             plt.axis('off')
         else:
@@ -95,72 +93,77 @@ def animate(U, D, savename='test', show=False):
         ims.append([im])
     ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True,
                                     repeat_delay=10)
-    ani.save(savename + '.gif', writer='imagemagick', fps=10)
+    ani.save(savename+'.gif', writer='imagemagick', fps=10)
     if show:
         plt.show()
     return ani
 
-
 def creategif(img):
-    Dp = downfromup.predict(img)[:, :, :].clip(0, 1)
-    animate(img, Dp, 'animatedted')
-    return 0
-
+  Dp=downfromup.predict(img)[:,:,:].clip(0,1)
+  animate(img, Dp,'animatedted')
+  return 0
 
 def convertvideo(videoname, modelconverter, outname, size=None):
-    logging.info(f"inputfilename: {videoname}")
-    logging.info(f"outputfilename: {outname}")
     vidcap = cv2.VideoCapture(videoname)
     success, image = vidcap.read()
     fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
     if size == None:
-        shapeout = (int(np.ceil(image.shape[1] / 32) * 32) * 2, int(np.ceil(image.shape[0] / 32) * 32))
+        shapeout = (int(np.ceil(image.shape[1] / 32) * 32), int(np.ceil(image.shape[0] / 32) * 32) * 2)
     else:
-        shapeout = (size[0] // 32 * 32 * 2, size[1] // 32 * 32)
+        shapeout = (size[0] // 32 * 32, size[1] // 32 * 32 * 2)
+
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     startflag = 0
     count = 0
     ctr = 0
     timein = time.time()
+
     while success:
         try:
             success, image = vidcap.read()
-            logging.info(f"frame count {count}")
+            print(count)
             count += 1
             ctr = ctr + 1
-            imageU = tf.image.resize_with_pad(image, shapeout[1], shapeout[0] // 2)
+
+            imageU = tf.image.resize_with_pad(image, shapeout[1] // 2, shapeout[0])
             U = imageU
             CC = np.expand_dims(np.transpose(np.indices((U.shape[0], U.shape[1])) / U.shape[0], (1, 2, 0)), 0)
             D = modelconverter.predict([np.stack([U / 255]), CC])[0, :, :, :].clip(0, 1)
-            frame = np.concatenate([U, D * 255], 1).astype(np.uint8)
+            #  D = D * 2.0  # Increase the depth range by multiplying with a factor
+            # D = np.exp(D)  # Apply an exponential scaling function to the depth values
+            # D = cv2.bilateralFilter(D, 9, 75, 75)  # Apply bilateral filtering to the depth map
+
+            frame = np.concatenate([U, D * 255], 0).astype(np.uint8)
+
             if startflag == 0:
                 out = cv2.VideoWriter(outname, fourcc, int(fps), (int(frame.shape[1]), int(frame.shape[0])))
                 startflag = 1
+
             out.write(frame)
+
             if ctr % (fps) == 0:
-                logging.info("%d s %.2f sec per videosecond", ctr // int(fps), time.time() - timein)
+                print(ctr // (fps), ' s ', time.time() - timein, ' sec per videosecond')
                 timein = time.time()
-                # break
-            # if ctr==10:
-            #  break
+
         except:
-            logging.error(traceback.format_exc())
+            print(print(traceback.format_exc()))
             break
+
     vidcap.release()
     out.release()
     return frame
 
-
 def convertimage(imagepath, converter):
-    img = cv2.imread(imagepath)
-    # flip channels from bgr to rgb
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = np.stack([img])
-    shapeout = (int(img.shape[1] // 32 - 0.000001) * 32 + 32, (int(img.shape[2] // 32 - 0.000001) * 32 + 32), 3)
-    img = img[:, :shapeout[0], :shapeout[1], :shapeout[2]]
-    CC = np.expand_dims(np.transpose(np.indices((img.shape[1], img.shape[2])) / img.shape[1], (1, 2, 0)), 0)
-    predictedimage = converter.predict([img, CC]).astype(np.uint8)
-    return img[0, :, :, :], predictedimage[0, :, :, :].clip(0, 255)
+  img=cv2.imread(imagepath)
+  #flip channels from bgr to rgb
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img=np.stack([img])
+  shapeout=(int(img.shape[1]//32-0.000001)*32+32,(int(img.shape[2]//32-0.000001)*32+32),3)
+  img=img[:,:shapeout[0],:shapeout[1],:shapeout[2]]
+  CC=np.expand_dims(np.transpose(np.indices((img.shape[1],img.shape[2]))/img.shape[1],(1,2,0)),0)
+  predictedimage=converter.predict([img, CC]).astype(np.uint8)
+  return img[0,:,:,:], predictedimage[0,:,:,:].clip(0,255)
 
 
 # initiate model
